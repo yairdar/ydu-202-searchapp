d62bd211ace97e9e5db50b06779934b97fa8fe30
Vitaliy <skrepach@gmail.com>
2022-05-10 23:08:43 +0500

    Add method for correct date in database

--- searchup/blocks/vit_db/play_db/connect_db.py
+++ searchup/blocks/vit_db/play_db/connect_db.py
@@ -1,5 +1,5 @@
 import csv
-from datetime import date
+from datetime import date, datetime
 
 import psycopg2
 from psycopg2 import Error
@@ -10,7 +10,7 @@ from pydantic import BaseModel
 class Movie(BaseModel):
     #id: int
     name: str
-    year_date: str
+    year_date: date
     budget: int #2
     #genres: str#3
     revenue: int #15
@@ -29,10 +29,10 @@ def create_table():
         cursor = connection.cursor()
         # SQL-for create new table
         # Id INT PRIMARY  KEY  AUTO_INCREMENT
-        create_table_query = '''CREATE TABLE second_part_movies
+        create_table_query = '''CREATE TABLE third_part_movies
                               (Id SERIAL PRIMARY  KEY,
                               Name TEXT NOT NULL,
-                              Year_date varchar,
+                              Year_date date,
                               Budget integer,
                               revenue bigint,
                               vote_average float
@@ -62,8 +62,8 @@ def add_row(movie, id):
         cursor = connection.cursor()
         # SQL-for fill new row
 
-        #print(f""" INSERT INTO movies (id, name, year_date, budget,revenue,vote_average) VALUES  ('{movie.name}', {movie.year_date}, {movie.budget}, {movie.revenue}, {movie.vote_average})""")
-        insert_query = f""" INSERT INTO second_part_movies (id, name, year_date, budget,revenue,vote_average) VALUES  ({id},'{movie.name}', {movie.year_date}, {movie.budget}, {movie.revenue}, {movie.vote_average})"""
+        print(f""" INSERT INTO third_part_movies (id, name, year_date, budget,revenue,vote_average) VALUES  ('{movie.name}', {movie.year_date}, {movie.budget}, {movie.revenue}, {movie.vote_average})""")
+        insert_query = f""" INSERT INTO third_part_movies (id, name, year_date, budget,revenue,vote_average) VALUES  ({id},'{movie.name}', '{movie.year_date}', {movie.budget}, {movie.revenue}, {movie.vote_average})"""
         cursor.execute(insert_query)
         connection.commit()
         print(f"Data add successfully number of row {id} ")
@@ -89,27 +89,36 @@ def fill_db(file_name_csv, first_index, limit):
                 if count == 0 or count < first_index:
                     count += 1
                 else:
+
                     movie: Movie = Movie(
                         name=row[8],
-                        year_date=row[14],
+                        year_date=data_convert(row[14]),
                         budget=int(row[2]),
                         #genres=row[3],
                         revenue=int(row[15]),
                         vote_average=float(row[22])
                     )
-                    count += 1
-                    add_row(movie, count)
+                    if movie.budget == 0:
+                        count += 1
+                    else:
+                        count += 1
+                        add_row(movie, count)
 
+def data_convert(str_date):
+    dateString = str_date
+    dateFormatter = "%Y-%m-%d"
+
+    return datetime.strptime(dateString, dateFormatter)
 
 
 if __name__ == "__main__":
     file_name_csv = "movies_metadata.csv"
 
-    create_table() # create table with need column
+    #create_table() # create table with need column
 
 
     #add_row(movie)
     #fill_db(file_name_csv, 100, 300)# When fill the table don't forget change table name
-    fill_db(file_name_csv, 500, 6000)# When fill the table don't forget change table name
+    fill_db(file_name_csv, 500,600)# When fill the table don't forget change table name
 
 
