6b9c45c5dd89c341bbaf8de1430cb45314286180
Vitaliy <skrepach@gmail.com>
2022-05-20 22:38:11 +0500

    chenge test, not end

--- searchup/blocks/djfedos_db/collections_manager.py
+++ searchup/blocks/djfedos_db/collections_manager.py
@@ -8,6 +8,7 @@ class CollectionsManager:
         if path_to_wdir == None:
             path_to_wdir = Path.cwd()
         self.wdir_p = Path(path_to_wdir)
+        self.wdir_p = Path(path_to_wdir)
         self.wdir_p.mkdir(exist_ok=True, parents=True)
 
 
@@ -23,89 +24,90 @@ class CollectionsManager:
         else:
             return None
 
-    def list_collections(self):
+    def list_collections(self):  # remove absolut path and chang to __wdir sub folders
         res = []
+        print(self.wdir_p)
         for i in self.wdir_p.iterdir():
             if i.is_dir():
                 res.append(i)
+        print(res)
         return res
 
 
-def test_list_collection(tmpdir):
+# def test_list_collection(tmpdir):
+#     # arrange
+#     col = CollectionsManager(tmpdir)
+#     col.add_collection(name = "one")
+#     col.add_collection(name = "two")
+#     col.add_collection(name = "three")
+#     expected_collections = {Path(tmpdir, "one"), Path(tmpdir, "two"), Path(tmpdir, "three")}
+#     # act
+#     actual = set(col.list_collections())
+#     # assert
+#     assert expected_collections == actual
+
+
+# def test_add_collection(tmpdir):
+#     # arrange
+#     col = CollectionsManager(tmpdir)
+#     name = "tmp_dir_test"
+#     p = Path(tmpdir, name)
+#     # act
+#     col.add_collection(name)
+#     # assert
+#     assert p.exists()
+
+
+# def test_add_to_collection_web_api():
+#     # arrange
+#     from fastapi import FastAPI
+#     from fastapi.testclient import TestClient
+#     from fapi_server import app
+#     client = TestClient(app)
+#     token1 = "tok_tester"
+#
+#     # test for adding 1 dir
+#     # act
+#     response = client.get("/collections/add_item/" + token1)
+#     data = response.json()
+#
+#     # assert
+#     assert response.status_code == 200
+#     assert data == token1
+
+# def test_get_collection_web_api():
+#     # arrange
+#     from fastapi.testclient import TestClient
+#     from fapi_server import app
+#     client = TestClient(app)
+#     token1 = "tok_tester"
+#     # act
+#     response = client.get("/collections/get_item/" + token1)
+#     data = response.json()
+#     # assert
+#     assert response.status_code == 200
+#     assert data["get collection. item_name_id"] == token1
+
+
+#run test_add_to_collection_web_api before this test
+def test_get_list_web_api():
     # arrange
-    col = CollectionsManager(tmpdir)
-    col.add_collection(name = "one")
-    col.add_collection(name = "two")
-    col.add_collection(name = "three")
-    expected_collections = {Path(tmpdir, "one"), Path(tmpdir, "two"), Path(tmpdir, "three")}
-    # act
-    actual = set(col.list_collections())
-    # assert
-    assert expected_collections == actual
-
-
-def test_add_collection(tmpdir):
-    # arrange
-    col = CollectionsManager(tmpdir)
-    name = "tmp_dir_test"
-    p = Path(tmpdir, name)
-    # act
-    col.add_collection(name)
-    # assert
-    assert p.exists()
-
-
-def test_add_to_collection_web_api():
-    # arrange
-    from fastapi import FastAPI
     from fastapi.testclient import TestClient
     from fapi_server import app
+    expected = Path.cwd()
     client = TestClient(app)
-    token1 = "tok_tester"
-
-    # test for adding 1 dir
-    # act
-    response = client.get("/collections/add_item/" + token1)
-    data = response.json()
-
-    # assert
-    assert response.status_code == 200
-    assert data == token1
-
-def test_get_collection_web_api():
-    # arrange
-    from fastapi.testclient import TestClient
-    from fapi_server import app
-    client = TestClient(app)
-    token1 = "tok_tester"
-    # act
-    response = client.get("/collections/get_item/" + token1)
-    data = response.json()
-    # assert
-    assert response.status_code == 200
-    assert data["get collection. item_name_id"] == token1
-
-
-def test_get_list_web_api(tmpdir):
-    # arrange
-    from fastapi.testclient import TestClient
-    from fapi_server import app
-    client = TestClient(app)
-    col = CollectionsManager(tmpdir)
-    col.add_collection(name = "one")
-    col.add_collection(name = "two")
-    col.add_collection(name = "three")
-    expected_collections = {Path(tmpdir, "one"), Path(tmpdir, "two"), Path(tmpdir, "three")}
     # act
     response = client.get("/collections/list")
-    data = response.json()
-    actual = set(col.list_collections())
+
+    actual = response.json()
     # assert
     assert response.status_code == 200
-    assert expected_collections == actual
+    assert expected in actual
+
 
 if __name__ == '__main__':
     name = "__wdir"
 
     col = CollectionsManager(name)
-    print(col.add_collection(name))
+    col.list_collections()
+    #print(col.add_collection(name))
